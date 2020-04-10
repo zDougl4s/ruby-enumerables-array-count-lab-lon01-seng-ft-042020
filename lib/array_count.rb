6c4eb466af410c5034d07e927c12fc82b082e9c2
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-10 23:05:01 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,8 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
 
-array.count do | element.class = String |
+array.count do | element |
+  element.class == String
 end
 end
 
