58ecd0cd3d1674472fcc31c36883f620e3d6f062
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-10 23:10:34 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -10,5 +10,5 @@ end
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
   array.count do | element |
-    element=== ""
+    element=== ()
 end
